0f1ef46b687bfa11e96cdc7c404e68d7ef93b22a
Nick Tolley <nic.m.tolley@gmail.com>
2022-04-17 02:08:47 -0700

    Implement feature function into posterior training code

--- code/train_rc_posterior.py
+++ code/train_rc_posterior.py
@@ -33,43 +33,56 @@ limits = list(prior_dict.values())
 
 # x_orig stores full waveform to be used for embedding
 x_orig, theta_orig = np.load(f'{data_path}/sbi_sims/x_sbi.npy'), np.load(f'{data_path}/sbi_sims/theta_sbi.npy')
+x_orig, theta_orig = x_orig[:n_sims, :], theta_orig[:n_sims, :]
 
 # Add noise for regularization
 noise_amp = 0.1
 noise = np.random.random(x_orig.shape) * noise_amp - (noise_amp / 2)
 x_orig_noise = x_orig + noise
 
-x_peak_noise = get_dataset_peaks(x_orig_noise)
-
 dt = sim_metadata['dt'] # Sampling interval used for simulation
 fs = (1/dt) * 1e3
-x_psd_noise, _ = get_dataset_psd(x_orig_noise, fs=fs)
 
-pca = PCA(n_components=10, random_state=rng_seed)
-pca.fit(x_orig_noise)
-x_pca_noise = pca.transform(x_orig_noise)
+def psd_peak_func(x_raw, fs):
+    x_psd = get_dataset_psd(x_raw, fs=fs, return_freq=False)
+    x_peak = get_dataset_peaks(x_raw)
+    return np.hstack([x_psd, x_peak])
 
+pca5 = PCA(n_components=5, random_state=rng_seed)
+pca5.fit(x_orig_noise)
+
+pca10 = PCA(n_components=10, random_state=rng_seed)
+pca10.fit(x_orig_noise)
+
+pca30 = PCA(n_components=30, random_state=rng_seed)
+pca30.fit(x_orig_noise)
 
 posterior_metadata = {'rng_seed': rng_seed, 'noise_amp': noise_amp, 'n_sims': n_sims, 'fs': fs}
 posterior_metadata_save_label = f'{data_path}/posteriors/rc_posterior_metadata.pkl'
 with open(posterior_metadata_save_label, 'wb') as output_file:
         dill.dump(posterior_metadata, output_file)
 
-input_type_list = {'raw_waveform_noise': {
-                       'x_train': x_orig_noise, 'embedding_func': torch.nn.Identity,
-                       'embedding_dict': dict()},
-                   'pca_noise': {
-                       'x_train': x_pca_noise, 'embedding_func': torch.nn.Identity,
-                       'embedding_dict': dict()},
-                   'peak_noise': {
-                       'x_train': x_peak_noise, 'embedding_func': torch.nn.Identity,
-                       'embedding_dict': dict()},
-                   'psd_noise': {
-                       'x_train': x_psd_noise, 'embedding_func': torch.nn.Identity,
-                       'embedding_dict': dict()},
-                   'psd_peak_noise': {
-                       'x_train': np.hstack([x_psd_noise, x_peak_noise]), 'embedding_func': torch.nn.Identity,
-                       'embedding_dict': dict()}}
+input_type_list = {'raw_waveform': {
+                       'embedding_func': torch.nn.Identity,
+                       'embedding_dict': dict(), 'feature_func': torch.nn.Identity},
+                   'pca5': {
+                       'embedding_func': torch.nn.Identity,
+                       'embedding_dict': dict(), 'feature_func': pca5.transform},
+                   'pca10': {
+                       'embedding_func': torch.nn.Identity,
+                       'embedding_dict': dict(), 'feature_func': pca10.transform},
+                   'pca30': {
+                       'embedding_func': torch.nn.Identity,
+                       'embedding_dict': dict(), 'feature_func': pca30.transform},
+                   'peak': {
+                       'embedding_func': torch.nn.Identity,
+                       'embedding_dict': dict(), 'feature_func': get_dataset_peaks},
+                   'psd': {
+                       'embedding_func': torch.nn.Identity,
+                       'embedding_dict': dict(), 'feature_func': partial(get_dataset_psd, fs=fs, return_freq=False)},
+                   'psd_peak': {
+                       'embedding_func': torch.nn.Identity,
+                       'embedding_dict': dict(), 'feature_func': partial(psd_peak_func, fs=fs)}}
 
 # Train a posterior for each input type and save state_dict
 for input_type, input_dict in input_type_list.items():
@@ -78,20 +91,14 @@ for input_type, input_dict in input_type_list.items():
 
     neural_posterior = sbi_utils.posterior_nn(model='maf', embedding_net=input_dict['embedding_func'](**input_dict['embedding_dict']))
     inference = sbi_inference.SNPE(prior=prior, density_estimator=neural_posterior, show_progress_bars=True, device=device)
-    x_train = torch.tensor(input_dict['x_train']).float()
-    theta_train = torch.tensor(theta_orig[:n_sims,:]).float()
+    x_train = torch.tensor(input_dict['feature_func'](x_orig_noise)).float()
+    theta_train = torch.tensor(theta_orig).float()
     if x_train.dim() == 1:
         x_train= x_train.reshape(-1, 1)
-
-    x_train = x_train[:n_sims, :]
 
     inference.append_simulations(theta_train, x_train, proposal=prior)
 
     nn_posterior = inference.train(num_atoms=10, training_batch_size=5000, use_combined_loss=True, discard_prior_samples=True, max_num_epochs=None, show_train_summary=True)
-
-    # Save parameters for posterior, remove training data
-    save_input_dict = input_dict.copy()
-    save_input_dict.pop('x_train')
 
     posterior_dict[dict_key] = {'posterior': nn_posterior.state_dict(),
                                 'n_params': n_params,
