6874c514d8dd8df0190f61018f954ed6c03493b8
Nick Tolley <nic.m.tolley@gmail.com>
2022-05-13 02:58:46 -0700

    Refactor sim and training code for hnn_rc

--- code/hnn_rc/generate_SBI_sims_hnn_rc.py
+++ code/hnn_rc/generate_SBI_sims_hnn_rc.py
@@ -1,23 +1,29 @@
 import sys
 sys.path.append('../')
 import os
-#sys.path.append(os.get_cwd())
-from utils import (linear_scale_forward, log_scale_forward, run_hnn_sim,
-                   simulator_hnn, hnn_rc_param_function, load_prerun_simulations)
+from utils import (linear_scale_forward, log_scale_forward,
+                   run_hnn_sim, hnn_rc_param_function, UniformPrior)
 from hnn_core import jones_2009_model
+
+nsbi_sims = 110_0
 
 net = jones_2009_model()
 net.clear_connectivity()
 
-# Number of simulations to run when sampling from prior
    
 save_path = '../../data/hnn_rc/'
+save_suffix = 'sbi'
     
 prior_dict = {'prox_weight': {'bounds': (-4, -3), 'rescale_function': log_scale_forward},
               'dist_weight': {'bounds': (-4, -3), 'rescale_function': log_scale_forward}, 
               'latency': {'bounds': (-75, 75), 'rescale_function': linear_scale_forward}}
+
+prior = UniformPrior(parameters=list(prior_dict.keys()))
+theta_samples = prior.sample((nsbi_sims,))
+
 tstop = 350
 
-run_hnn_sim(net=net, param_function=hnn_rc_param_function, prior_dict=prior_dict, tstop=tstop, save_path=save_path)
+run_hnn_sim(net=net, param_function=hnn_rc_param_function, prior_dict=prior_dict,
+            theta_samples=theta_samples, tstop=tstop, save_path=save_path, save_suffix=save_suffix)
 
-os.system('scancel -u ntolley')
+#os.system('scancel -u ntolley')
